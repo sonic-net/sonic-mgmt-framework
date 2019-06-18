f986911ff99d1e4727e752ca622c768f26861cfb
jghalam <joe.ghalam@gmail.com>
2019-06-18 02:12:08 -0700

    Dell ACL PoC using Klish
    * Basic directory structure
    
    * Initial code commit for CLI Infra with ACL changes.
    
    * Support for ACL show commands
    
    * Support for interface config in-order to bind an ACL table.
    
    * Changes in the pre-processed XMLs for the ones which are dependent on ACL PoC.
    
    * Cleaned up the supported XML files.
    
    * Bug-fixes in the code wrt new CLI Infra added.
    
    * fix Makefile for klish
    
    * Support for the show commands as part of XML scripts, moved cli_types to sonic_types.
    
    * - Added following support to process raw CLI XMLs to produce exanded XMLs
      that Klish parser engine can consume to create the UI :
       1.) For each view and nested views add default commands like endi,exit
       2.) Substitute macros and nested macros refrenced in CLI xml with actual contents
       3.) Proper substitution of arguments passed to macros
       4.) Add pipe option to all show/get commands
       5.) Entity/Enumeration substitutions
    - Reorganized source as well as target directory structure
    
    * clearing unnecessary cli xmls from clitree/cli-xml
    
    * Render template files for ACL CLI commands (Arlo POC)
    
    * update XML files for POC
    
    * Support for all the commands with XML processing from CLI Infra and some cleanups in the un-used IP commands.
    
    * update make
    
    * More cleanup of the unwanted commands and un-supported definitions.
    
    * Added support for DTD that validates processed CLI XMLs with against a defined schema
    
    * Turned on schema validation for generic platform based entities
    clitree Makefile modification to remove pointing to old clis in pre-expanded form

---
+++ src/CLI/clitree/scripts/klish_platform_features_process.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+#set -x
+# Validate all platform xml files
+# For all platform_*.xml, run xmllint feature_master.xsd $i.xml
+# Create entities_platform.xml and features_platform.xml
+# For all platforms_*/xml, run xsltproc $i.xml with entities.xml and features.xsl
+# Copy a clish_prepare.py to sysroot
+# Run clish_prepare.py with first platform and update entities
+#
+# Run xsltproc on feature_master.xsd to create an xml file of all features - fullfeatures.xml
+#
+# During clish start:
+#    a. Open fullfeatures.xml to populate a list of features vs enabled-flag mappping.
+#    b. While preparing the pfeature list, consult this list instead of consulting the list of #defines
+#    c. Report errors on processing the fullfeatures.xml file
+
+PLATFORM_CONFIGS=platform_dummy.xml
+BUILD_DIR=$2/tmp
+PARSER_XML_PATH=$2
+ENTITIES_TEMPLATE=$1/mgmt_clish_entities.xsl
+FEATURES_MASTER=$1/mgmt_clish_feature_master.xsd
+
+function insert_in()
+{
+    # Insert in file - $1 with $value set in calling routine
+    filename=$1
+    outfile=$filename.bak
+    grep -q DOCTYPE $filename
+    # If there are ENTITY definitions, add it as part of it
+    # Else add a new DOCTYPE
+    if [ $? -eq 0 ]; then
+        option=1
+        matchpattern=".*DOCTYPE.*"
+        printvalue="${value}"
+    else
+        option=2
+        matchpattern="<?xml .*"
+        pref='<!DOCTYPE CLISH_MODULE ['
+        printvalue="${pref}${value} ]>"
+    fi
+    #echo Insert_in $filename. Option $option
+    while read -r line; do
+        echo ${line} >> $outfile
+        if [[ "${line}" =~ ${matchpattern} ]]; then
+            #echo Match found for ${line}
+            echo "${printvalue}" >> $outfile
+            #set +x
+        fi
+    done < $filename
+    #set -x
+    mv -f $outfile $filename
+}
+
+# Do a simple text based insertion of the feature-val entities
+# TBD - Replace them with an xml parser based insertion in future, if required
+function insert_entities()
+{
+    value=`cat $1`
+    parser=$2
+    echo insert_entities: $1 $parser
+    list=`echo ${parser}/*.xml`
+    for i in ${list}; do
+        echo Processing $i
+        insert_in $i
+        xmllint $i >& /dev/null
+        if [ $? -ne 0 ]; then
+            echo ENTITY insertion in $i failed
+            exit 1
+        fi
+    done
+    #echo $i
+    #insert_in $i
+}
+
+
+echo Sanity check of platform config files with feature master ...
+    xmllint --schema $FEATURES_MASTER $PLATFORM_CONFIGS >& /dev/null
+    if [ $? -ne 0 ]; then
+        echo Failed to validate $PLATFORM_CONFIGS
+        exit 1
+    fi
+
+mkdir -p ${BUILD_DIR}
+echo Done. Generating platform specific files ...
+    base=${PLATFORM_CONFIGS%*.xml}         # Strip of the .xml suffix
+    platform=${base#platform_*} # Get the platform name
+    xsltproc $ENTITIES_TEMPLATE $PLATFORM_CONFIGS > $BUILD_DIR/${platform}_entities.ent #2>/dev/null
+    # echo ${platform}_entities.ent ready
+    if [ $? -ne 0 ]; then
+        echo Failed to apply entities xsl template for $PLATFORM_CONFIGS
+        exit 1
+    fi
+echo Done
+
+# Use the last platform's file for compilation purpose
+pwd=${PWD}
+cd ${PARSER_XML_PATH}
+echo Inserting platform features
+insert_entities ${BUILD_DIR}/${platform}_entities.ent ${PARSER_XML_PATH}/command-tree
+cp ${BUILD_DIR}/*.xml ${PARSER_XML_PATH}/command-tree
+
+rm -r ${BUILD_DIR}
+exit 0
