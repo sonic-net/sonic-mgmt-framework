fd3baff9a84d4e681c0128c03e66a8eebecfce22
Prabhu Sreenivasan <ps944501@lvnvde0930.lvn.broadcom.net>
2019-07-08 04:14:27 -0700

    fixed go binary path issue

--- src/translib/ocbinds/oc.go
+++ src/translib/ocbinds/oc.go
@@ -1,3 +1,3 @@
 package ocbinds
 
-//go:generate sh -c "/usr/local/go1.12/bin/go run /tmp/go/src/github.com/openconfig/ygot/generator/generator.go -generate_fakeroot -output_file ocbinds.go -package_name ocbinds -generate_fakeroot -fakeroot_name=device -compress_paths=false -exclude_modules ietf-interfaces -path . $(find ../../../models/yang -name '*.yang' | sort)" 
+//go:generate sh -c "/usr/local/go/bin/go run /tmp/go/src/github.com/openconfig/ygot/generator/generator.go -generate_fakeroot -output_file ocbinds.go -package_name ocbinds -generate_fakeroot -fakeroot_name=device -compress_paths=false -exclude_modules ietf-interfaces -path . $(find ../../../models/yang -name '*.yang' | sort)"
