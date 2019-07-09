716ce6f8d999f19ccc5a6643eebbb26c3c22aef7
Asha Behera <asha.behera@broadcom.com>
2019-07-09 03:29:59 -0700

    CVL UT framework .
    Change-Id: I8e4c8e480c8bb2076701a6c4cba0a2d27dc5f470

---
+++ src/cvl/tests/jsondata_test.go
@@ -0,0 +1,41 @@
+package cvl_test
+
+var json_edit_config_create_acl_table_dependent_data = []string{`{
+							"stage": "INGRESS",
+							"type": "L3"
+						}`}
+
+var json_edit_config_create_acl_rule_config_data = []string{
+						`{
+        						"PACKET_ACTION": "FORWARD",
+               						 "SRC_IP": "10.1.1.1/32",
+                					"L4_SRC_PORT": "1909",
+               						 "IP_PROTOCOL": "103",
+              						  "DST_IP": "20.2.2.2/32",
+               						 "L4_DST_PORT_RANGE": "9000-12000"
+
+
+						}`}
+
+var json_validate_config_data = []string{`{
+					"INTERFACE": {
+					"Ethernet668|10.0.0.0/31": {},
+					"Ethernet24|10.0.0.2/31": {},
+					"Ethernet112|10.0.0.4/31": {}
+					}
+				}`,
+				`{
+					"DEVICE_METADATA": {
+						"localhost": {
+						"hwsku": "Force10-S6100",
+						"default_bgp_status": "up",
+						"docker_routing_config_mode": "unified",
+						"hostname": "sonic-s6100-01",
+						"platform": "x86_64-dell_s6100_c2538-r0",
+						"mac": "4c:76:25:f4:70:82",
+						"default_pfcwd_status": "disable",
+						"deployment_id": "1",
+						"type": "ToRRouter"
+						}
+					}
+				}`}
