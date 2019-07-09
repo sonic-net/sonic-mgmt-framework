fcbfbead162397a15501fd69d1f0db70d986ceb1
Balachandar Mani <balachandar.mani@broadcom.com>
2019-07-09 16:02:36 -0700

    Added unit test code for request_binder and path_utils
    Change-Id: Ic0d19894982ebbc8b0e65fac029686f732de9a5e

--- src/translib/ocbinds/oc.go
+++ src/translib/ocbinds/oc.go
@@ -1,3 +1,3 @@
 package ocbinds
 
-//go:generate sh -c "/usr/local/go/bin/go run /tmp/go/src/github.com/openconfig/ygot/generator/generator.go -generate_fakeroot -output_file ocbinds.go -package_name ocbinds -generate_fakeroot -fakeroot_name=device -compress_paths=false -exclude_modules ietf-interfaces -path . $(find ../../../models/yang -name '*.yang' | sort)"
+//go:generate sh -c "/tools/oss/packages/x86_64-rhel6/go/1.12.5/bin/go run /projects/csg_sonic/bm408846/gopath/src/github.com/openconfig/ygot/generator/generator.go -generate_fakeroot -output_file ocbinds.go -package_name ocbinds -generate_fakeroot -fakeroot_name=device -compress_paths=false -exclude_modules ietf-interfaces -path . $(find ../../../models/yang -name '*.yang' | sort)"
