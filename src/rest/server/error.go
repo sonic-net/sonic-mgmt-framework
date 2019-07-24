28af49450df1c9193599fe193767db6e958de073
Sachin Holla <sachin.holla@broadcom.com>
2019-07-24 15:23:22 +0530

    Error translation for translib and db errors
    Change-Id: I595d5d5f0848b1644e709a0b53b9bba01fe1a027

--- src/rest/server/error.go
+++ src/rest/server/error.go
@@ -11,6 +11,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"cvl"
+	"translib/tlerr"
 )
 
 // errorResponse defines the RESTCONF compliant error response
@@ -24,12 +27,30 @@ type errorResponse struct {
 // errorInfo defines the RESTCONF compilant error information
 // payload.
 type errorInfo struct {
-	Type    string `json:"error-type"`
-	Tag     string `json:"error-tag"`
-	AppTag  string `json:"error-app-tag,omitempty"`
-	Path    string `json:"error-path,omitempty"`
-	Message string `json:"error-message,omitempty"`
+	Type    errtype `json:"error-type"`
+	Tag     errtag  `json:"error-tag"`
+	AppTag  string  `json:"error-app-tag,omitempty"`
+	Path    string  `json:"error-path,omitempty"`
+	Message string  `json:"error-message,omitempty"`
 }
+
+type errtype string
+type errtag string
+
+const (
+	// error-type values
+	errtypeProtocol    errtype = "protocol"
+	errtypeApplication errtype = "application"
+
+	// error-tag values
+	errtagInvalidValue          errtag = "invalid-value"
+	errtagOperationFailed       errtag = "operation-failed"
+	errtagOperationNotSupported errtag = "operation-not-supported"
+	errtagAccessDenied          errtag = "access-denied"
+	errtagResourceDenied        errtag = "resource-denied"
+	errtagInUse                 errtag = "in-use"
+	errtagMalformedMessage      errtag = "malformed-message"
+)
 
 // httpErrorType is an error structure for indicating HTTP protocol
 // errors. Includes HTTP status code and user displayable message.
@@ -60,25 +81,69 @@ func httpServerError(msg string, args ...interface{}) error {
 // for an error object. Response payalod is formatted as per RESTCONF
 // specification (RFC8040, section 7.1). Uses json encoding.
 func prepareErrorResponse(err error, r *http.Request) (status int, data []byte, mimeType string) {
-	var errInfo errorInfo
-
-	switch e := err.(type) {
-	case httpErrorType:
-		status = e.status
-		errInfo.Type = "protocol"
-		errInfo.Message = e.message
-
-	default:
-		//FIXME
-		status = http.StatusBadRequest
-		errInfo.Type = "application"
-		errInfo.Message = err.Error()
-	}
-
+	status, errInfo := toErrorInfo(err, r)
 	var resp errorResponse
 	resp.Err.Arr = append(resp.Err.Arr, errInfo)
 	data, _ = json.Marshal(&resp)
 	mimeType = "application/yang-data+json"
+	return
+}
+
+// formatError translates an error object into HTTP status and an
+//  errorInfo object.
+func toErrorInfo(err error, r *http.Request) (status int, errInfo errorInfo) {
+	switch e := err.(type) {
+	case httpErrorType:
+		status = e.status
+		errInfo.Type = errtypeProtocol
+		errInfo.Message = e.message
+
+		// Guess error app tag from http status code
+		switch status {
+		case http.StatusBadRequest: // 400
+			errInfo.Tag = errtagInvalidValue
+		case http.StatusUnauthorized: // 401
+			errInfo.Tag = errtagAccessDenied
+		case http.StatusForbidden: // 403
+			errInfo.Tag = errtagAccessDenied
+		case http.StatusNotFound: // 404
+			errInfo.Tag = errtagInvalidValue
+		case http.StatusMethodNotAllowed: // 405
+			errInfo.Tag = errtagOperationNotSupported
+		default: // 5xx and others
+			errInfo.Tag = errtagOperationFailed
+		}
+
+	case tlerr.TranslibRedisClientEntryNotExist:
+		status = http.StatusNotFound
+		errInfo.Type = errtypeApplication
+		errInfo.Tag = errtagInvalidValue
+		errInfo.Message = "Entry not found"
+
+	case tlerr.TranslibCVLFailure:
+		status = http.StatusInternalServerError
+		errInfo.Type = errtypeApplication
+		errInfo.Tag = errtagInvalidValue
+		errInfo.Message = e.CVLErrorInfo.ConstraintErrMsg
+		errInfo.AppTag = e.CVLErrorInfo.ErrAppTag
+
+		switch cvl.CVLRetCode(e.Code) {
+		case cvl.CVL_SEMANTIC_KEY_ALREADY_EXIST, cvl.CVL_SEMANTIC_KEY_DUPLICATE:
+			status = http.StatusConflict
+			errInfo.Tag = errtagResourceDenied
+			errInfo.Message = "Entry already exists"
+
+		case cvl.CVL_SEMANTIC_KEY_NOT_EXIST:
+			status = http.StatusNotFound
+			errInfo.Tag = errtagInvalidValue
+			errInfo.Message = "Entry not found"
+		}
+
+	default:
+		status = http.StatusInternalServerError
+		errInfo.Type = errtypeApplication
+		errInfo.Tag = errtagOperationFailed
+	}
 
 	return
 }
