94027644062626eaf2a3f2c316bebe8ee53478ce
Anand Kumar Subramanian <anand.subramanian@broadcom.com>
2019-07-29 16:30:11 -0700

    Subscribe support checkin 3

--- src/translib/subscribe.go
+++ src/translib/subscribe.go
@@ -20,13 +20,14 @@ package translib
 import (
 	"sync"
 	"time"
+	"bytes"
 	"translib/db"
 	log "github.com/golang/glog"
 	"github.com/Workiva/go-datastructures/queue"
 )
 
 //Subscribe mutex for all the subscribe operations on the maps to be thread safe
-var subscribeMutex = &sync.Mutex{}
+var sMutex = &sync.Mutex{}
 
 type notificationInfo struct{
 	table               db.TableSpec
@@ -34,26 +35,27 @@ type notificationInfo struct{
 	dbno				db.DBNum
 	needCache			bool
 	path				string
-	app					*appInterface
-	cache				[]byte
-	sDB					*db.DB //Subscription DB should be used only for keyspace notification
+	app				   *appInterface
+	appInfo			   *appInfo
+	cache			  []byte
+	dbs [db.MaxDB]	   *db.DB //used to perform get operations
+	sDB				   *db.DB //Subscription DB should be used only for keyspace notification
 }
 
 type subscribeInfo struct{
 	syncDone			bool
 	q				   *queue.PriorityQueue
-	nInfo				[]*notificationInfo
+	nInfoArr		 []*notificationInfo
 	stop				chan struct{}
-	dbs [db.MaxDB]	   *db.DB //used to perform get operations
 }
 
-var notificationMap map[*db.SKey]*notificationInfo
-var subscribeMap map[*notificationInfo]*subscribeInfo
+var nMap map[*db.SKey]*notificationInfo
+var sMap map[*notificationInfo]*subscribeInfo
 var stopMap map[chan struct{}]*subscribeInfo
 
 func init() {
-	notificationMap = make(map[*db.SKey]*notificationInfo)
-	subscribeMap = make(map[*notificationInfo]*subscribeInfo)
+	nMap = make(map[*db.SKey]*notificationInfo)
+	sMap = make(map[*notificationInfo]*subscribeInfo)
 	stopMap	= make(map[chan struct{}]*subscribeInfo)
 }
 
@@ -73,35 +75,157 @@ func runSubscribe(q *queue.PriorityQueue) error {
 	return err
 }
 
-func startSubscribe(opt db.Options, nInfoList []*notificationInfo, sInfo *subscribeInfo) error {
-	var skey *db.SKey
+func startDBSubscribe(opt db.Options, nInfoList []*notificationInfo, sInfo *subscribeInfo) error {
 	var sKeyList []*db.SKey
-	var nInfo *notificationInfo
 
-	subscribeMutex.Lock()
-
-	for _, nInfo = range nInfoList {
-		skey = &db.SKey{ Ts: &nInfo.table, Key: &nInfo.key}
+	for _, nInfo := range nInfoList {
+		skey := &db.SKey{ Ts: &nInfo.table, Key: &nInfo.key}
 		sKeyList = append(sKeyList, skey)
-		notificationMap[skey] = nInfo
-		subscribeMap[nInfo] = sInfo
+		nMap[skey] = nInfo
+		sMap[nInfo] = sInfo
 	}
 
 	sDB, err := db.SubscribeDB(opt, sKeyList, notificationHandler)
 
 	if err == nil {
-		for _, nInfo = range nInfoList {
+		for _, nInfo := range nInfoList {
 			nInfo.sDB = sDB
+		}
+	} else {
+		for i, nInfo := range nInfoList {
+			delete(nMap, sKeyList[i])
+			delete(sMap, nInfo)
 		}
 	}
 
-	subscribeMutex.Unlock()
 	return err
 }
 
-func notificationHandler(d *db.DB, skey *db.SKey, key *db.Key, event db.SEvent) error {
-    log.Info("notificationHandler: d: ", d, " skey: ", *skey, " key: ", *key,
+func notificationHandler(d *db.DB, sKey *db.SKey, key *db.Key, event db.SEvent) error {
+    log.Info("notificationHandler: d: ", d, " skey: ", *sKey, " key: ", *key,
         " event: ", event)
+    sMutex.Lock()
+    defer sMutex.Unlock()
+
+	if sKey != nil {
+		nInfo := nMap[sKey]
+		sInfo := sMap[nInfo]
+
+		isChanged := isCacheChanged(nInfo)
+
+		if isChanged {
+			sendNotification(sInfo, nInfo)
+		}
+	}
+
     return nil
 }
 
+func updateCache(nInfo *notificationInfo) error {
+	var err error
+
+	json, err1 := getJson (nInfo)
+
+	if err1 == nil {
+		nInfo.cache = json
+	} else {
+		err = err1
+	}
+
+	return err
+}
+
+func isCacheChanged(nInfo *notificationInfo) bool {
+	json, err := getJson (nInfo)
+
+    if err == nil {
+        if bytes.Equal(nInfo.cache, json) {
+			return false
+		} else {
+			nInfo.cache = json
+			return true
+		}
+    }
+	return false
+}
+
+func startSubscribe(sInfo *subscribeInfo, dbNotificationMap map[db.DBNum][]*notificationInfo) error {
+	var err error
+
+    sMutex.Lock()
+	defer sMutex.Unlock()
+
+    for dbno, nInfoArr := range dbNotificationMap {
+        opt := getDBOptions(dbno)
+        err = startDBSubscribe(opt, nInfoArr, sInfo)
+		if err != nil {
+			//TODO:Need to cleanup old subscriptions
+			return err
+		}
+        sInfo.nInfoArr = append(sInfo.nInfoArr, nInfoArr...)
+    }
+
+	stopMap[sInfo.stop] = sInfo
+
+    for i, nInfo := range sInfo.nInfoArr {
+
+        err = updateCache(nInfo)
+
+		if err != nil {
+			//TODO:Need to cleanup all subscriptions
+            return err
+        }
+
+		if i == len(sInfo.nInfoArr) {
+			sInfo.syncDone = true
+		}
+
+		sendNotification(sInfo, nInfo)
+    }
+	sInfo.syncDone = true
+
+	return err
+}
+
+func getJson (nInfo *notificationInfo) ([]byte, error) {
+    var payload []byte
+
+	app := nInfo.app
+	path := nInfo.path
+	appInfo := nInfo.appInfo
+
+    err := appInitialize(app, appInfo, path, nil, GET)
+
+    if  err != nil {
+        return payload, err
+    }
+
+	dbs := nInfo.dbs
+
+    err = (*app).translateGet (dbs)
+
+    if err != nil {
+        return payload, err
+    }
+
+    resp, err := (*app).processGet(dbs)
+
+    if err == nil {
+        payload = resp.Payload
+    }
+
+    return payload, err
+}
+
+func sendNotification(sInfo *subscribeInfo, nInfo *notificationInfo){
+	sInfo.q.Put(&SubscribeResponse{
+			Path:nInfo.path,
+			Payload:nInfo.cache,
+			Timestamp:    time.Now().UnixNano(),
+			SyncComplete: sInfo.syncDone,
+	})
+}
+
+func stophandler(stop chan struct{}) {
+	
+}
