cf22b3779af763c19545bce4f0465a2ae1f50937
Haemanthi Sree <haemanthisree@gmail.com>
2019-08-05 11:44:56 -0700

    IP Address Validation Fix, Change of Delete IP Address API (#63)
    * Updated the YANG Object
    
    * Updated ipv4 and ipv6 xml files with the new YANG objects.

--- src/CLI/actioner/sonic-cli-if.py
+++ src/CLI/actioner/sonic-cli-if.py
@@ -46,9 +46,9 @@ def generate_body(func, args):
        sp = args[1].split('/')
        keypath = [ args[0], 0, sp[0] ]
        body = { "openconfig-if-ip:config":  {"ip" : sp[0], "prefix-length" : int(sp[1])} }
-    elif func.__name__ == 'delete_openconfig_if_ip_interfaces_interface_subinterfaces_subinterface_ipv4_addresses_address_config':
+    elif func.__name__ == 'delete_openconfig_if_ip_interfaces_interface_subinterfaces_subinterface_ipv4_addresses_address_config_prefix_length':
        keypath = [args[0], 0, args[1]]
-    elif func.__name__ == 'delete_openconfig_if_ip_interfaces_interface_subinterfaces_subinterface_ipv6_addresses_address_config':
+    elif func.__name__ == 'delete_openconfig_if_ip_interfaces_interface_subinterfaces_subinterface_ipv6_addresses_address_config_prefix_length':
        keypath = [args[0], 0, args[1]]
     elif func.__name__ == 'get_openconfig_interfaces_interfaces_interface':
 	keypath = [args[0]]
