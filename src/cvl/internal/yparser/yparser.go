99720f5d5ccdad86529a4617032b49cf508c9d3f
Partha Dutta <partha.dutta@broadcom.com>
2019-09-18 09:37:06 +0530

    Moving unwanted CVL YANG to testdata

--- src/cvl/internal/yparser/yparser.go
+++ src/cvl/internal/yparser/yparser.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	log "github.com/golang/glog"
 	. "cvl/internal/util"
+	"fmt"
 )
 
 /*
@@ -235,7 +236,10 @@ func ParseSchemaFile(modelFile string) (*YParserModule, YParserError) {
 
 	module :=  C.lys_parse_path((*C.struct_ly_ctx)(ypCtx), C.CString(modelFile), C.LYS_IN_YIN)
 	if module == nil {
+		fmt.Printf("\n\nFailed Parsed file .... %s\n\n", modelFile)
 		return nil, getErrorDetails()
+	} else {
+		fmt.Printf("\n\nParsed file .... %s\n\n", modelFile)
 	}
 
 	if (strings.Contains(modelFile, "sonic-common.yin") == true) {
@@ -250,7 +254,12 @@ func ParseSchemaFile(modelFile string) (*YParserModule, YParserError) {
 //Add child node to a parent node
 func(yp *YParser) AddChildNode(module *YParserModule, parent *YParserNode, name string) *YParserNode {
 
-	return (*YParserNode)(C.lyd_new((*C.struct_lyd_node)(parent), (*C.struct_lys_module)(module), C.CString(name)))
+	//return (*YParserNode)(C.lyd_new((*C.struct_lyd_node)(parent), (*C.struct_lys_module)(module), C.CString(name)))
+	ret := (*YParserNode)(C.lyd_new((*C.struct_lyd_node)(parent), (*C.struct_lys_module)(module), C.CString(name)))
+	if (ret == nil) {
+		fmt.Printf("\n\nFailed parsing  .... %s\n\n", name)
+	}
+	return ret
 }
 
 //Add child node to a parent node
