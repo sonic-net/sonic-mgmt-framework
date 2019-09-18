0aa77421242e96e7170e32e07e276bcaafb18df0
Prabhu Sreenivasan <ps944501@lvnvde0930.lvn.broadcom.net>
2019-09-18 03:35:09 -0700

    [JIRA SONIC-9659] Merge Transformer changes by Dell
    Change-Id: I477e967a8f28efb6fc6aeb6a93850b79c121f33f

--- src/translib/app_interface.go
+++ src/translib/app_interface.go
@@ -125,12 +125,9 @@ func getAppModuleInfo(path string) (*appInfo, error) {
 		return app, err
 	}
 
-	errStr := "Unsupported path=" + path
-
-	err = errors.New(errStr)
-	log.Error(errStr)
-
-	var app *appInfo
+	/* If no specific app registered fallback to default/common app */
+	log.Infof("No app module registered for path %s hence fallback to default/common app", path)
+	app := appMap["*"]
 
 	return app, err
 }
