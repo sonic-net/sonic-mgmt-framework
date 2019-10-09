710ce6eb46928ab0de56dc25bb142c5f12321a86
Balachandar Mani <balachandar.mani@broadcom.com>
2019-10-09 11:27:57 -0700

    [JIRA SONIC-10047] Made changes to fix the issues in the binding for the PATCH/PUT method with the target node as map/LIST
    Change-Id: Iba68f2fbdd8fa01ece4e158e892ad7c2f0bd3dc5

--- src/translib/request_binder_test.go
+++ src/translib/request_binder_test.go
@@ -200,6 +200,13 @@ func TestUnMarshallUri(t *testing.T) {
 		payload:     []byte{},
 		appRootType: reflect.TypeOf(ocbinds.OpenconfigAcl_Acl{}),
 		want:        "5",
+		//	}, {
+		//		tid:         15,
+		//		uri:         "/openconfig-bgp:bgp/neighbors/neighbor[neighbor-address=30.0.0.1]/",
+		//		opcode:      2,
+		//		payload:     []byte{},
+		//		appRootType: reflect.TypeOf(ocbinds.OpenconfigBgp_Bgp{}),
+		//		want:        "5",
 	}}
 
 	for _, tt := range tests {
@@ -211,6 +218,12 @@ func TestUnMarshallUri(t *testing.T) {
 			if strings.Contains(err.Error(), tt.want) == false {
 				t.Error("Error in unmarshalling the URI: didn't get the expected error, and the error string is", err)
 			}
+			//		} else if tt.tid == 15 {
+			//			if neighborEntry, ok := (*workObj).(*ocbinds.OpenconfigBgp_Bgp_Neighbors_Neighbor); ok && *neighborEntry.NeighborAddress == "30.0.0.1" {
+			//				fmt.Println("PASSED testcase 15 : neighborEntry.NeighborAddress => ", *neighborEntry.NeighborAddress)
+			//			} else {
+			//				t.Error("Error in unmarshalling the URI: OpenconfigBgp_Bgp_Neighbors_Neighbor - object casting failed")
+			//			}
 		} else if tt.tid == 14 {
 			leafList, ok := (*workObj).([]ocbinds.E_OpenconfigPacketMatchTypes_TCP_FLAGS)
 			fmt.Println("leaf list target node value is :", leafList)
@@ -459,6 +472,34 @@ func TestUnMarshall(t *testing.T) {
 		payload:     []byte("{ \"acl-entries\": { \"acl-entry\": [  {  \"sequence-id\": abc, \"config\": { \"sequence-id\": 1, \"description\": \"Description for MyACL1 Rule Seq 1\"  },   \"ipv4\": {  \"config\": {  \"source-address\": \"11.1.1.1/32\",  \"destination-address\": \"21.1.1.1/32\", \"dscp\": 1, \"protocol\": \"IP_TCP\" } }, \"transport\": { \"config\": { \"source-port\": 101, \"destination-port\": 201 } }, \"actions\": { \"config\": { \"forwarding-action\": \"ACCEPT\" } } } ] } } "),
 		appRootType: reflect.TypeOf(ocbinds.OpenconfigAcl_Acl{}),
 		want:        "failed to create map value for insert",
+		//	}, {
+		//		tid:         14, // PATCH - test bgp neighbor bind with update
+		//		uri:         "/openconfig-bgp:bgp/neighbors/neighbor[neighbor-address=30.0.0.1]/ebgp-multihop/config/multihop-ttl",
+		//		opcode:      4,
+		//		payload:     []byte("{\"openconfig-bgp:multihop-ttl\": 3}"),
+		//		appRootType: reflect.TypeOf(ocbinds.OpenconfigBgp_Bgp{}),
+		//		want:        "15",
+	}, {
+		tid:         15, // PATCH - from the base node acl
+		uri:         "/openconfig-acl:acl/",
+		opcode:      4,
+		payload:     []byte("{  \"openconfig-acl:acl\": {  \"acl-sets\": {  \"acl-set\": [    {      \"name\": \"MyACL3\",    \"type\": \"ACL_IPV4\",   \"config\": {   \"name\": \"MyACL3\",                 \"type\": \"ACL_IPV4\",                 \"description\": \"Description for MyACL3\" }    }   ] } } }"),
+		appRootType: reflect.TypeOf(ocbinds.OpenconfigAcl_Acl{}),
+		want:        "XXX",
+	}, {
+		tid:         16, // PATCH - from the base node acl
+		uri:         "/openconfig-acl:acl/acl-sets/acl-set",
+		opcode:      4,
+		payload:     []byte("{    \"acl-set\": [    {      \"name\": \"MyACL3\",    \"type\": \"ACL_IPV4\",   \"config\": {   \"name\": \"MyACL3\",                 \"type\": \"ACL_IPV4\",                 \"description\": \"Description for MyACL3\" }    }   ] } "),
+		appRootType: reflect.TypeOf(ocbinds.OpenconfigAcl_Acl{}),
+		want:        "XXX",
+	}, {
+		tid:         17, // PATCH - from the base node acl
+		uri:         "/openconfig-acl:acl/acl-sets/openconfig-acl:acl-set[name=MyACL333][type=ACL_IPV4]/",
+		opcode:      4,
+		payload:     []byte("{    \"acl-set\": [    {      \"name\": \"MyACL333\",    \"type\": \"ACL_IPV4\",   \"config\": {   \"name\": \"MyACL333\",                 \"type\": \"ACL_IPV4\",                 \"description\": \"Description for MyACL333\" }    }   ] } "),
+		appRootType: reflect.TypeOf(ocbinds.OpenconfigAcl_Acl{}),
+		want:        "XXX",
 	}}
 
 	for _, tt := range tests {
@@ -466,10 +507,37 @@ func TestUnMarshall(t *testing.T) {
 
 		if err != nil {
 			if strings.Contains(err.Error(), tt.want) == false {
-				t.Error("Error in unmarshalling the payload: didn't get the expected error, and the error string is", err)
+				t.Errorf("TestUnMarshall: Testcase id: %d Error in unmarshalling the payload: didn't get the expected error, and the error string is %s ", tt.tid, err)
 			}
 		} else {
-			if tt.tid == 4 {
+			if tt.tid == 17 {
+				fmt.Println("TestUnMarshall: Testcase 17 passed - workObj: ", *workObj)
+				if aclSet, ok := (*workObj).(*ocbinds.OpenconfigAcl_Acl_AclSets_AclSet); ok {
+					fmt.Printf("TestUnMarshall: Testcase 17 passed - Acl Name: %s, and Acl Type: %d\n", *aclSet.Name, aclSet.Type)
+				} else {
+					t.Error("TestUnMarshall: Testcase 17 - Error in unmarshalling the payload: OpenconfigAcl_Acl_AclSets")
+				}
+			} else if tt.tid == 16 {
+				if reflect.ValueOf(*workObj).Kind().String() == "map" {
+					fmt.Println("TestUnMarshall: Testcase 16 passed - workobj: ", *workObj)
+				} else {
+					t.Error("TestUnMarshall: Testcase 16 - Error in unmarshalling the payload: OpenconfigAcl_AclSet")
+				}
+			} else if tt.tid == 15 {
+				aclObj, ok := (*workObj).(*ocbinds.OpenconfigAcl_Acl)
+				if ok == true {
+					fmt.Println("TestUnMarshall: Testcase 15 passed - ", aclObj)
+				} else {
+					t.Error("TestUnMarshall: Testcase 15 - Error in unmarshalling the payload: OpenconfigAcl_Acl")
+				}
+			} else if tt.tid == 14 {
+				ttlVal, ok := (*workObj).(*uint8)
+				if ok == false || *ttlVal != 3 {
+					t.Error("TestUnMarshall: Testcase 14 - Error in unmarshalling the payload: OpenconfigBgp_Bgp_Neighbors_Neighbor_EbgpMultihop_Config_multihop-ttl failed")
+				} else {
+					fmt.Println("TestUnMarshall: Testcase 14 passed - ptach method: => multihop-ttl value ==> ", *ttlVal)
+				}
+			} else if tt.tid == 4 {
 				aclSet, ok := (*workObj).(*ocbinds.OpenconfigAcl_Acl_AclSets_AclSet)
 				if ok == true {
 					if aclSet.AclEntries.AclEntry[1] != nil && *aclSet.AclEntries.AclEntry[1].SequenceId == 1 {
