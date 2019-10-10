9df719d37a379815f3e10fd2111f9d8503f64c76
Ranjini Nagaraj <ranjini_nagaraj@dell.com>
2019-10-09 18:07:34 -0700

    Add Copyright info

--- src/translib/transformer/xlate_to_db.go
+++ src/translib/transformer/xlate_to_db.go
@@ -1,3 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  Copyright 2019 Dell, Inc.                                                 //
+//                                                                            //
+//  Licensed under the Apache License, Version 2.0 (the "License");           //
+//  you may not use this file except in compliance with the License.          //
+//  You may obtain a copy of the License at                                   //
+//                                                                            //
+//  http://www.apache.org/licenses/LICENSE-2.0                                //
+//                                                                            //
+//  Unless required by applicable law or agreed to in writing, software       //
+//  distributed under the License is distributed on an "AS IS" BASIS,         //
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  //
+//  See the License for the specific language governing permissions and       //
+//  limitations under the License.                                            //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
 package transformer
 
 import (
@@ -314,7 +332,7 @@ func sonicYangReqToDbMapDelete(xpathPrefix string, tableName string, keyName str
                fieldName := tokens[len(tokens)-1]
                dbSpecField := tableName + "/" + fieldName
                _, ok := xDbSpecMap[dbSpecField]
-               if ok && xDbSpecMap[dbSpecField].fieldType == "leaf" {
+               if ok && xDbSpecMap[dbSpecField].fieldType == "leaf" || xDbSpecMap[dbSpecField].fieldType == "leaf-list" {
                        // Specific leaf case
                         dbVal.Field = make(map[string]string)
                         dbVal.Field[fieldName] = ""
