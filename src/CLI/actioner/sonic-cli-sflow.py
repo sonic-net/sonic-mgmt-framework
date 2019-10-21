f5081f61627526fcb1cbcf4ac9b497810cc6536d
Garrick He <garrick_he@dell.com>
2019-10-21 12:45:14 -0700

    Annotate sFlow YANG + update actioner script
    * Annotate sFlow YANG to query applDB for session information.
    * Update sFlow actioner script.
    
    Signed-off-by: Garrick He <garrick_he@dell.com>

--- src/CLI/actioner/sonic-cli-sflow.py
+++ src/CLI/actioner/sonic-cli-sflow.py
@@ -3,8 +3,12 @@ import sys
 import time
 import json
 import ast
+from rpipe_utils import pipestr
 from collections import OrderedDict
 from scripts.render_cli import show_cli_output
+import sonic_sflow_client
+from sonic_sflow_client.api.sonic_sflow_api import SonicSflowApi  
+from sonic_sflow_client.rest import ApiException
 
 
 import urllib3
@@ -28,7 +32,27 @@ def get_sflow_interface():
     pass
 
 def run(func, args):
+    c = sonic_sflow_client.Configuration()
+    c.verify_ssl = False
+    aa = sonic_sflow_client.SonicSflowApi(api_client=sonic_sflow_client.ApiClient(configuration=c))
+    keypath = []
+    api_response = getattr(aa,'get_sonic_sflow_sonic_sflow')(*keypath)
+    print(api_response)
+    api_response = getattr(aa, 'get_sonic_sflow_sonic_sflow_sflow_session')(*keypath)
+    print(api_response)
+    keypath = ['col1']
+    body = {  "sonic-sflow:SFLOW_COLLECTOR_LIST": [
+               {
+                   "collector_name": 'col1',
+                   "collector_ip": "5.5.5.5",
+                   "collector_port": 54321
+               }] }
 
+    api_response = getattr(aa, 'patch_sonic_sflow_sonic_sflow_sflow_collector_sflow_collector_list')(*keypath, body=body)
+    print(api_response)
+    keypath = []
+    api_response = getattr(aa,'get_sonic_sflow_sonic_sflow')(*keypath)
+    print(api_response)
     # create a body block
     if (func.__name__ == 'get_sflow'):
         sflow_info = {'sflow' : {'admin_state' : 'enabled', 'polling-interval' : 20, 'agent-id' : 'default'}}
@@ -41,5 +65,6 @@ def run(func, args):
     return
 
 if __name__ == '__main__':
-    func = eval(sys.argv[1], globals(), {})
+    pipestr().write(sys.argv)
+    func = eval(sys.argv[1], globals(), sonic_sflow_client.SonicSflowApi.__dict__)
     run(func, sys.argv[2:])
