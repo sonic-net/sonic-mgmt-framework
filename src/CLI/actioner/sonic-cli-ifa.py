7f9cdd5730dd2aed99a35356adc8339fb26a57bf
Srinadh Penugonda <Srinadh_Penugondaa@dell.com>
2019-10-21 13:03:48 -0700

    IFA-drop-1: Support for set/delete for IFA_DEVICE_TABLE

---
+++ src/CLI/actioner/sonic-cli-ifa.py
@@ -0,0 +1,111 @@
+import sys
+import time
+import json
+import ast
+import sonic_ifa_client
+from sonic_ifa_client.rest import ApiException
+from scripts.render_cli import show_cli_output
+
+import urllib3
+urllib3.disable_warnings()
+
+
+plugins = dict()
+
+def register(func):
+    """Register sdk client method as a plug-in"""
+    plugins[func.__name__] = func
+    return func
+
+
+def call_method(name, args):
+    method = plugins[name]
+    return method(args)
+
+def generate_body(func, args):
+    body = None
+    keypath = []
+
+    # Get the rules of all ECN table entries.
+    if func.__name__ == 'get_sonic_wred_profile_sonic_wred_profile_wred_profile':
+        keypath = [args[0]]
+    elif func.__name__ == 'get_sonic_wred_profile_sonic_wred_profile':
+        keypath = []
+    elif func.__name__ == 'patch_sonic_ifa_sonic_ifa_ifa_device_table_ifa_device_table_list_deviceid':
+       keypath = [ args[0] ]
+       body = { "sonic-ifa:deviceid": ((int) args[1]) }
+    elif func.__name__ == 'patch_sonic_ifa_sonic_ifa_ifa_device_table_ifa_device_table_list_devicetype':
+       keypath = [args[0]]
+       body = { "sonic-ifa:devicetype": args[1] }
+    elif func.__name__ == 'delete_sonic_ifa_sonic_ifa_ifa_device_table_ifa_device_table_list_devicetype':
+       keypath = [args[0]]
+    elif func.__name__ == 'delete_sonic_ifa_sonic_ifa_ifa_device_table_ifa_device_table_list_deviceid':
+       keypath = [args[0]]
+    else:
+       body = {}
+
+    return keypath,body
+
+def run(func, args):
+
+    c = sonic_ifa_client.Configuration()
+    c.verify_ssl = False
+    aa = sonic_ifa_client.SonicIfaApi(api_client=sonic_ifa_client.ApiClient(configuration=c))
+
+    # create a body block
+    keypath, body = generate_body(func, args)
+
+    try:
+        if body is not None:
+           api_response = getattr(aa,func.__name__)(*keypath, body=body)
+        else :
+           api_response = getattr(aa,func.__name__)(*keypath)
+
+        if api_response is None:
+            print ("Success")
+        else:
+            # Get Command Output
+            api_response = aa.api_client.sanitize_for_serialization(api_response)
+            if 'sonic-ifa:sonic-ifa' in api_response:
+                value = api_response['sonic-ifa:sonic-ifa']
+                if 'IFA_DEVICE_TABLE' in value:
+                    tup = value['IFA_DEVICE_TABLE']
+
+            if api_response is None:
+                print("Failed")
+            else:
+                if func.__name__ == 'get_sonic_wred_profile_sonic_wred_profile_wred_profile_wred_profile_list':
+                     show_cli_output(args[1], api_response)
+                elif func.__name__ == 'get_sonic_wred_profile_sonic_wred_profile':
+                     show_cli_output(args[0], api_response)
+                else:
+                     return
+    except ApiException as e:
+        print("Exception when calling get_sonic_ifa ->%s : %s\n" %(func.__name__, e))
+        if e.body != "":
+            body = json.loads(e.body)
+            if "ietf-restconf:errors" in body:
+                 err = body["ietf-restconf:errors"]
+                 if "error" in err:
+                     errList = err["error"]
+
+                     errDict = {}
+                     for dict in errList:
+                         for k, v in dict.iteritems():
+                              errDict[k] = v
+
+                     if "error-message" in errDict:
+                         print "%Error: " + errDict["error-message"]
+                         return
+                     print "%Error: Transaction Failure"
+                     return
+            print "%Error: Transaction Failure"
+        else:
+            print "Failed"
+
+if __name__ == '__main__':
+
+    func = eval(sys.argv[1], globals(), sonic_ifa_client.SonicIfaApi.__dict__)
+
+    run(func, sys.argv[2:])
+
