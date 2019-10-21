166bbd6f6ab8ffc68715a3a29fa0c6f91d2a1832
Rathnasabapathy Velautharaj <rathna_sabapathy@dell.com>
2019-10-21 01:26:15 -0700

    initial commit for bgp-nbrs-state get from bgp-container

--- src/translib/transformer/xfmr_bgp.go
+++ src/translib/transformer/xfmr_bgp.go
@@ -2,12 +2,15 @@ package transformer
 
 import (
     "errors"
+    "encoding/json"
     "translib/ocbinds"
     "reflect"
+    "os/exec"
     log "github.com/golang/glog"
+    ygot "github.com/openconfig/ygot/ygot"
 )
 
-func getBgpRoot (inParams XfmrParams) (*ocbinds.OpenconfigNetworkInstance_NetworkInstances_NetworkInstance_Protocols_Protocol_Bgp, error) {
+func getBgpRoot (inParams XfmrParams) (*ocbinds.OpenconfigNetworkInstance_NetworkInstances_NetworkInstance_Protocols_Protocol_Bgp, string, error) {
     pathInfo := NewPathInfo(inParams.uri)
     niName := pathInfo.Var("name")
     bgpId := pathInfo.Var("identifier")
@@ -15,31 +18,31 @@ func getBgpRoot (inParams XfmrParams) (*ocbinds.OpenconfigNetworkInstance_Networ
     var err error
 
     if len(niName) == 0 {
-        return nil, errors.New("Network-instance-name missing")
+        return nil, "", errors.New("Network-instance-name missing")
     }
 
     if bgpId != "BGP" {
-        return nil, errors.New("Network-instance-name missing")
+        return nil, "", errors.New("Network-instance-name missing")
     }
 
     if len(protoName) == 0 {
-        return nil, errors.New("Network-instance-name missing")
+        return nil, "", errors.New("Network-instance-name missing")
     }
 
 	deviceObj := (*inParams.ygRoot).(*ocbinds.Device)
     netInstsObj := deviceObj.NetworkInstances
 
     if netInstsObj.NetworkInstance == nil {
-        return nil, errors.New("Network-instance-name missing")
+        return nil, "", errors.New("Network-instance-name missing")
     }
 
     netInstObj := netInstsObj.NetworkInstance[niName]
     if netInstObj == nil {
-        return nil, errors.New("Network-instance-name missing")
+        return nil, "", errors.New("Network-instance-name missing")
     }
 
     if netInstObj.Protocols == nil || len(netInstObj.Protocols.Protocol) == 0 {
-        return nil, errors.New("Network-instance-name missing")
+        return nil, "", errors.New("Network-instance-name missing")
     }
 
     var protoKey ocbinds.OpenconfigNetworkInstance_NetworkInstances_NetworkInstance_Protocols_Protocol_Key
@@ -47,11 +50,10 @@ func getBgpRoot (inParams XfmrParams) (*ocbinds.OpenconfigNetworkInstance_Networ
     protoKey.Name1 = protoName
     protoInstObj := netInstObj.Protocols.Protocol[protoKey]
     if protoInstObj == nil {
-        return nil, errors.New("Network-instance-name missing")
+        return nil, "", errors.New("Network-instance-name missing")
     }
-    return protoInstObj.Bgp, err
+    return protoInstObj.Bgp, niName, err
 }
-
 
 func init () {
     XlateFuncBind("YangToDb_bgp_gbl_tbl_key_xfmr", YangToDb_bgp_gbl_tbl_key_xfmr)
@@ -62,19 +64,8 @@ func init () {
     XlateFuncBind("DbToYang_bgp_allow_multiple_as_xfmr", DbToYang_bgp_allow_multiple_as_xfmr)
     XlateFuncBind("YangToDb_bgp_graceful_restart_status_xfmr", YangToDb_bgp_graceful_restart_status_xfmr)
     XlateFuncBind("DbToYang_bgp_graceful_restart_status_xfmr", DbToYang_bgp_graceful_restart_status_xfmr)
-/*    XlateFuncBind("DbToYang_bgp_nbrs_nbr_state_xfmr", DbToYang_bgp_nbrs_nbr_state_xfmr) */
+    XlateFuncBind("DbToYang_bgp_nbrs_nbr_state_xfmr", DbToYang_bgp_nbrs_nbr_state_xfmr)
 }
-
-/*
-func getBgpRoot (inParams XfmrParams) *ocbinds.OpenconfigNetworkInstance_NetworkInstances_NetworkInstance_Protocols_Protocol_Bgp, error {
-    pathInfo := NewPathInfo(inParams.uri)
-    niName := pathInfo.Var("name")
-    if len(niName) == 0 {return "", errors.New("Network-instance-name missing")}
-
-    deviceObj := (*inParams.ygRoot).(*ocbinds.Device)
-    return deviceObj.Lacp
-}
-*/
 
 var YangToDb_bgp_gbl_tbl_key_xfmr KeyXfmrYangToDb = func(inParams XfmrParams) (string, error) {
     var err error
@@ -225,11 +216,128 @@ var DbToYang_bgp_graceful_restart_status_xfmr FieldXfmrDbtoYang = func(inParams 
     return result, err
 }
 
-/*
-var DbToYang_bgp_nbrs_nbr_state_xfmr SubTreeXfmrDbToYang = func(inParams XfmrParams) error {
-    pathInfo := NewPathInfo(inParams.uri)
-    vrfName := pathInfo.Var("name")
+func exec_vtysh_cmd (vtysh_cmd string) (map[string]interface{}, error) {
+    var err error
+    oper_err := errors.New("Opertational error")
 
+    cmd := exec.Command("docker", "exec", "bgp", "vtysh -c \"", vtysh_cmd, "\"")
+    out_stream, err := cmd.StdoutPipe()
+    if err != nil {
+        log.Fatalf("Can't get stdout pipe: %s\n", err)
+        return nil, oper_err
+    }
+    err = cmd.Start()
+    if err != nil {
+        log.Fatalf("cmd.Start() failed with %s\n", err)
+        return nil, oper_err
+    }
 
+    var outputJson map[string]interface{}
+
+    err = json.NewDecoder(out_stream).Decode(&outputJson)
+    if err != nil {
+        log.Fatalf("Not able to decode teamd json output: %s\n", err)
+        return nil, oper_err
+    }
+
+    err = cmd.Wait()
+    if err != nil {
+        log.Fatalf("Command execution completion failed with %s\n", err)
+        return nil, oper_err
+    }
+
+    return outputJson, err
 }
-*/
+
+func fill_nbr_state_info (nbrAddr string, nbrDataValue interface{},
+                          nbr_obj *ocbinds.OpenconfigNetworkInstance_NetworkInstances_NetworkInstance_Protocols_Protocol_Bgp_Neighbors_Neighbor) {
+    nbr_obj.State.NeighborAddress = &nbrAddr
+
+    nbrDataJson := nbrDataValue.(map[string]interface{})
+
+    bgp_session_state := nbrDataJson["bgpState"]
+    if bgp_session_state == "Established" {
+        nbr_obj.State.SessionState = ocbinds.OpenconfigBgp_Bgp_Neighbors_Neighbor_State_SessionState_ESTABLISHED
+    }
+}
+
+func get_specific_nbr_state (nbrs_obj *ocbinds.OpenconfigNetworkInstance_NetworkInstances_NetworkInstance_Protocols_Protocol_Bgp_Neighbors,
+                             niName string, nbrAddr string) error {
+    var err error
+
+    vtysh_cmd := "show ip bgp vrf " + niName + " neighbors " + nbrAddr + " json"
+    nbrMapJson, cmd_err := exec_vtysh_cmd (vtysh_cmd)
+    if cmd_err != nil {
+        log.Fatalf("Failed to fetch bgp neighbors state info for niName:%s nbrAddr:%s. Err: %s\n", niName, nbrAddr, err)
+        return cmd_err
+    }
+
+    var ok bool
+    var nbr_obj *ocbinds.OpenconfigNetworkInstance_NetworkInstances_NetworkInstance_Protocols_Protocol_Bgp_Neighbors_Neighbor
+    if len(nbrs_obj.Neighbor) == 0 {
+        nbr_obj, _ = nbrs_obj.NewNeighbor (nbrAddr)
+    } else {
+        if nbr_obj, ok = nbrs_obj.Neighbor[nbrAddr] ; !ok {
+            nbr_obj, _ = nbrs_obj.NewNeighbor (nbrAddr)
+        }
+    }
+    ygot.BuildEmptyTree(nbr_obj)
+
+    nbrDataJson := nbrMapJson[nbrAddr].(map[string]interface{})
+    fill_nbr_state_info (nbrAddr, nbrDataJson, nbr_obj)
+
+    return err
+}
+
+func get_all_nbr_state (nbrs_obj *ocbinds.OpenconfigNetworkInstance_NetworkInstances_NetworkInstance_Protocols_Protocol_Bgp_Neighbors,
+                        niName string) error {
+    var err error
+
+    vtysh_cmd := "show ip bgp vrf " + niName + " neighbors " + " json"
+    nbrsMapJson, cmd_err := exec_vtysh_cmd (vtysh_cmd)
+    if cmd_err != nil {
+        log.Fatalf("Failed to fetch all bgp neighbors state info for niName:%s. Err: %s\n", niName, err)
+        return cmd_err
+    }
+
+    for nbrAddr, nbrDataJson := range nbrsMapJson {
+        nbr_obj, _ := nbrs_obj.NewNeighbor (nbrAddr)
+        ygot.BuildEmptyTree(nbr_obj)
+        fill_nbr_state_info (nbrAddr, nbrDataJson, nbr_obj)
+    }
+
+    return err
+}
+
+var DbToYang_bgp_nbrs_nbr_state_xfmr SubTreeXfmrDbToYang = func(inParams XfmrParams) error {
+    var err error
+    oper_err := errors.New("Opertational error")
+    cmn_log := "GET: xfmr for BGP-nbrs state"
+
+    bgp_obj, niName, err := getBgpRoot (inParams)
+    if err != nil {
+        log.Errorf ("%s failed !! Error:%s", cmn_log , err);
+        return err;
+    }
+
+    nbrs_obj := bgp_obj.Neighbors
+    if nbrs_obj == nil {
+        log.Fatalf("Neighbors container missing")
+        return oper_err
+    }
+
+    pathInfo := NewPathInfo(inParams.uri)
+    targetUriPath, err := getYangPathFromUri(pathInfo.Path)
+    nbrAddr := pathInfo.Var("NeighborAddress")
+
+    log.Infof("%s : path:%s; template:%s targetUriPath:%s niName:%s nbrAddr:%s",
+              cmn_log, pathInfo.Path, pathInfo.Template, targetUriPath, niName, nbrAddr)
+
+    if len(nbrAddr) != 0 {
+        err = get_specific_nbr_state (nbrs_obj, niName, nbrAddr);
+    } else {
+        err = get_all_nbr_state (nbrs_obj, niName);
+    }
+
+    return err;
+}
