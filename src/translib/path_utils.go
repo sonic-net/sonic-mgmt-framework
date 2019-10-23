7f3c44ae26b809a0320b192a5b10a447607a05f3
Sachin Holla <sachin.holla@broadcom.com>
2019-10-23 23:14:02 +0530

    [JIRA SONIC-10486] Handle duplicate path param names in NewPathInfo
    NewPathInfo() API was not handling duplicate path parameters; first
    parameter entry gets overwritten in PathInfo.Vars map. This is changed
    to suffix "#N" to each duplicate parameter. N is the number of
    occurrences of that parameter name from the left.
    
    Example:
    Path: /one[xx=1]/two[yy=2]/three[xx=3]/four[zz=4]/five[yy=5]/six[xx=6]
    Vars: { "xx":"1", "yy":"2", "xx#2":"3", "zz":"4", "yy#2":"5", "xx#3":"6" }
    
    Change-Id: Ic110bd7940b96f3c5cd00f5b0e764f60318b08ad

--- src/translib/path_utils.go
+++ src/translib/path_utils.go
@@ -41,6 +41,12 @@ type PathInfo struct {
 	Vars     map[string]string
 }
 
+// HasVar checks if the PathInfo contains given variable.
+func (p *PathInfo) HasVar(name string) bool {
+	_, exists := p.Vars[name]
+	return exists
+}
+
 // Var returns the string value for a path variable. Returns
 // empty string if no such variable exists.
 func (p *PathInfo) Var(name string) string {
@@ -90,6 +96,16 @@ func NewPathInfo(path string) *PathInfo {
 
 		name := readUntil(r, '=')
 		value := readUntil(r, ']')
+
+		// Handle duplicate parameter names by suffixing "#N" to it.
+		// N is the number of occurance of that parameter name.
+		if info.HasVar(name) {
+			namePrefix := name
+			for k := 2; info.HasVar(name); k++ {
+				name = fmt.Sprintf("%s#%d", namePrefix, k)
+			}
+		}
+
 		if len(name) != 0 {
 			fmt.Fprintf(&template, "{}")
 			info.Vars[name] = value
@@ -188,5 +204,3 @@ func getObjectFieldName(targetUri *string, deviceObj *ocbinds.Device, ygotTarget
 	}
 	return "", errors.New("Target object not found")
 }
-
-
