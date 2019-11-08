6472ea19e1b4fc41bcc8307a1f0d6555098cc93a
Srinadh Penugonda <Srinadh_Penugondaa@dell.com>
2019-11-08 13:56:51 -0800

    address review comments: separated tam configuration to different yang, xml, actioner and renderer Display collector and flow information in tabular form

--- src/CLI/actioner/sonic-cli-tam.py
+++ src/CLI/actioner/sonic-cli-tam.py
@@ -2,8 +2,8 @@ import sys
 import time
 import json
 import ast
-import sonic_ifa_client
-from sonic_ifa_client.rest import ApiException
+import sonic_tam_client
+from sonic_tam_client.rest import ApiException
 from scripts.render_cli import show_cli_output
 
 import urllib3
@@ -27,32 +27,27 @@ def generate_body(func, args):
     keypath = []
 
     # Set/Get the rules of all IFA table entries.
-    if func.__name__ == 'get_sonic_ifa_sonic_ifa_tam_int_ifa_feature_table':
+    if func.__name__ == 'get_sonic_tam_sonic_tam_tam_device_table':
        keypath = []
-    elif func.__name__ == 'get_sonic_ifa_sonic_ifa_tam_int_ifa_flow_table':
+    elif func.__name__ == 'get_sonic_tam_sonic_tam_tam_collector_table':
        keypath = []
-    elif func.__name__ == 'get_sonic_ifa_sonic_ifa_tam_int_ifa_flow_table_tam_int_ifa_flow_table_list':
+    elif func.__name__ == 'get_sonic_tam_sonic_tam_tam_collector_table_tam_collector_table_list':
        keypath = [args[0]]
-    elif func.__name__ == 'patch_sonic_ifa_sonic_ifa_tam_int_ifa_feature_table_tam_int_ifa_feature_table_list_enable':
+    elif func.__name__ == 'patch_sonic_tam_sonic_tam_tam_device_table_tam_device_table_list_deviceid':
        keypath = [ args[0] ]
-       if args[1] == 'True':
-           body = { "sonic-ifa:enable": True }
-       else:
-           body = { "sonic-ifa:enable": False }
-    elif func.__name__ == 'patch_sonic_ifa_sonic_ifa_tam_int_ifa_flow_table_tam_int_ifa_flow_table_list':
-       keypath = [ args[0] ]
-       bodydict = {"name": args[0], "acl-rule-name": args[1], "acl-table-name": args[2]}
-       for i in range(len(args)):
-           if args[i] == "sv":
-               if args[i+1] != "cv":
-                   bodydict["sampling-rate"] = int(args[i+1])
-           elif args[i] == "cv":
-               if i+1 < len(args):
-                   bodydict["collector-name"] = args[i+1]
-           else:
-               pass
-       body = { "sonic-ifa:TAM_INT_IFA_FLOW_TABLE_LIST": [ bodydict ] }
-    elif func.__name__ == 'delete_sonic_ifa_sonic_ifa_tam_int_ifa_flow_table_tam_int_ifa_flow_table_list':
+       body = { "sonic-tam:deviceid": int(args[1]) }
+    elif func.__name__ == 'delete_sonic_tam_sonic_tam_tam_device_table_tam_device_table_list_deviceid':
+       keypath = [args[0]]
+    elif func.__name__ == 'patch_list_sonic_tam_sonic_tam_tam_collector_table_tam_collector_table_list':
+       keypath = [ ]
+       body = {
+           "sonic-tam:TAM_COLLECTOR_TABLE_LIST": [
+              {
+                  "name": args[0], "ipaddress-type": args[1], "ipaddress": args[2], "port": int(args[3])
+              }
+           ]
+       }
+    elif func.__name__ == 'delete_sonic_tam_sonic_tam_tam_collector_table_tam_collector_table_list':
        keypath = [ args[0] ]
     else:
        body = {}
@@ -61,9 +56,9 @@ def generate_body(func, args):
 
 def run(func, args):
 
-    c = sonic_ifa_client.Configuration()
+    c = sonic_tam_client.Configuration()
     c.verify_ssl = False
-    aa = sonic_ifa_client.SonicIfaApi(api_client=sonic_ifa_client.ApiClient(configuration=c))
+    aa = sonic_tam_client.SonicTamApi(api_client=sonic_tam_client.ApiClient(configuration=c))
 
     # create a body block
     keypath, body = generate_body(func, args)
@@ -79,10 +74,14 @@ def run(func, args):
         else:
             # Get Command Output
             api_response = aa.api_client.sanitize_for_serialization(api_response)
-            if 'sonic-ifa:sonic-ifa' in api_response:
-                value = api_response['sonic-ifa:sonic-ifa']
-                if 'TAM_INT_IFA_FEATURE_TABLE' in value:
+            if 'sonic-tam:sonic-tam' in api_response:
+                value = api_response['sonic-tam:sonic-tam']
+                if 'TAM_DEVICE_TABLE' in value:
+                    tup = value['TAM_DEVICE_TABLE']
+                elif 'TAM_INT_IFA_FEATURE_TABLE' in value:
                     tup = value['TAM_INT_IFA_FEATURE_TABLE']
+                elif 'TAM_COLLECTOR_TABLE' in value:
+                    tup = value['TAM_COLLECTOR_TABLE']
                 elif 'TAM_INT_IFA_FLOW_TABLE' in value:
                     tup = value['TAM_INT_IFA_FLOW_TABLE']
                 else:
@@ -90,16 +89,16 @@ def run(func, args):
 
             if api_response is None:
                 print("Failed")
-            elif func.__name__ == 'get_sonic_ifa_sonic_ifa_tam_int_ifa_feature_table':
+            elif func.__name__ == 'get_sonic_tam_sonic_tam_tam_device_table':
                 show_cli_output(args[0], api_response)
-            elif func.__name__ == 'get_sonic_ifa_sonic_ifa_tam_int_ifa_flow_table':
+            elif func.__name__ == 'get_sonic_tam_sonic_tam_tam_collector_table':
                 show_cli_output(args[0], api_response)
-            elif func.__name__ == 'get_sonic_ifa_sonic_ifa_tam_int_ifa_flow_table_tam_int_ifa_flow_table_list':
+            elif func.__name__ == 'get_sonic_tam_sonic_tam_tam_collector_table_tam_collector_table_list':
                 show_cli_output(args[1], api_response)
             else:
                 return
     except ApiException as e:
-        print("Exception when calling get_sonic_ifa ->%s : %s\n" %(func.__name__, e))
+        print("Exception when calling get_sonic_tam ->%s : %s\n" %(func.__name__, e))
         if e.body != "":
             body = json.loads(e.body)
             if "ietf-restconf:errors" in body:
@@ -123,7 +122,7 @@ def run(func, args):
 
 if __name__ == '__main__':
 
-    func = eval(sys.argv[1], globals(), sonic_ifa_client.SonicIfaApi.__dict__)
+    func = eval(sys.argv[1], globals(), sonic_tam_client.SonicTamApi.__dict__)
 
     run(func, sys.argv[2:])
 
