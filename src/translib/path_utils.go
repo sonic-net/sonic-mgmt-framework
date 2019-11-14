fedd987be5c5eaf48458bae44d5d00a58641ba60
Ashok Daparthi <Ashok_Daparthi@Dell.com>
2019-11-13 16:58:48 -0800

    Resolbves

--- src/translib/path_utils.go
+++ src/translib/path_utils.go
@@ -43,8 +43,8 @@ type PathInfo struct {
 
 // HasVar checks if the PathInfo contains given variable.
 func (p *PathInfo) HasVar(name string) bool {
-   _, exists := p.Vars[name]
-   return exists
+	_, exists := p.Vars[name]
+	return exists
 }
 
 // Var returns the string value for a path variable. Returns
@@ -97,14 +97,14 @@ func NewPathInfo(path string) *PathInfo {
 		name := readUntil(r, '=')
 		value := readUntil(r, ']')
 
-               // Handle duplicate parameter names by suffixing "#N" to it.
-               // N is the number of occurance of that parameter name.
-               if info.HasVar(name) {
-                   namePrefix := name
-                   for k := 2; info.HasVar(name); k++ {
-                       name = fmt.Sprintf("%s#%d", namePrefix, k)
-                   }
-               }
+		// Handle duplicate parameter names by suffixing "#N" to it.
+		// N is the number of occurance of that parameter name.
+		if info.HasVar(name) {
+			namePrefix := name
+			for k := 2; info.HasVar(name); k++ {
+				name = fmt.Sprintf("%s#%d", namePrefix, k)
+			}
+		}
 
 		if len(name) != 0 {
 			fmt.Fprintf(&template, "{}")
