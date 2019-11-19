9773ca492c8fc042ae0cc93d2499574083802c19
Karthikeyan Ananthakrishnan <karthikeyan.ananthakrishnan@broadcom.com>
2019-11-19 00:34:32 -0800

    [JIRA SONIC-11192] KLISH CLI changes for show per vlan neighbour status
    Change-Id: Ie11ba1749b921a0d69cac712f36156744c568bf7

--- src/CLI/actioner/sonic-cli-vlan.py
+++ src/CLI/actioner/sonic-cli-vlan.py
@@ -27,6 +27,7 @@ from rpipe_utils import pipestr
 from scripts.render_cli import show_cli_output
 
 vlanDict = {}
+suppressVlanList = []
 
 class ifInfo:
     ifModeDict = {}
@@ -43,6 +44,10 @@ def invoke_api(func, args=[]):
 
     if func == 'get_sonic_vlan_sonic_vlan':
         path = cc.Path('/restconf/data/sonic-vlan:sonic-vlan')
+        return api.get(path)
+
+    if func == 'get_sonic_vxlan_sonic_vxlan_suppress_vlan_neigh':
+        path = cc.Path('/restconf/data/sonic-vxlan:sonic-vxlan')
         return api.get(path)
 
     return api.cli_not_implemented(func)
@@ -108,6 +113,26 @@ def updateVlanInfoMap(vlanTuple, vlanId):
             ifData = vlanDict.get(vlanName)
             ifData.oper_status = operStatus
 
+def updateVlanNeighSuppressMap(suppTuple, tunnelMapTuple, vlanId):
+    sDict = {}
+    tempDict = {}
+    num = 0
+    for dict in tunnelMapTuple:
+        vid = dict['vlan']
+        num += 1
+        if vlanId:
+            if(vid != vlanId):
+                continue
+        sDict['name'] = vid
+        tempDict['name'] = vid
+        tempDict['suppress'] = 'on'
+        if tempDict in suppTuple:
+            sDict['suppress'] = "on"
+        else:
+            sDict['suppress'] = "off"
+        sDict['netdev'] = dict['name'] + "-" + vid[4:]
+        suppressVlanList.append(sDict.copy())
+ 
 def run(func, args):
     response = invoke_api(func, args)
 
@@ -127,6 +152,15 @@ def run(func, args):
                     if 'VLAN_TABLE_LIST' in vlanCont:
                          vlanTup = vlanCont['VLAN_TABLE_LIST']
                          updateVlanInfoMap(vlanTup, args[0])
+
+            if 'sonic-vxlan:sonic-vxlan' in api_response:
+                value = api_response['sonic-vxlan:sonic-vxlan']
+                suppressCont = value['SUPPRESS_VLAN_NEIGH']
+                vxlanCont = value['VXLAN_TUNNEL_MAP']
+                suppTuple = suppressCont['SUPPRESS_VLAN_NEIGH_LIST']
+                vxlanTuple = vxlanCont['VXLAN_TUNNEL_MAP_LIST']
+                updateVlanNeighSuppressMap(suppTuple,vxlanTuple, args[0])
+
             if api_response is None:
                 print("Failed")
             else:
@@ -139,6 +173,8 @@ def run(func, args):
                 vDictSorted = collections.OrderedDict(sorted(vDict.items(), key = lambda t: getVlanId(t[0])))
                 if func == 'get_sonic_vlan_sonic_vlan':
                      show_cli_output(args[1], vDictSorted)
+                elif func == 'get_sonic_vxlan_sonic_vxlan_suppress_vlan_neigh':
+                     show_cli_output(args[1], suppressVlanList)
                 else:
                      return
 
