60533a33b0f3b6870bd5773f07ad592afaed526c
Srinadh Penugonda <Srinadh_Penugondaa@dell.com>
2019-12-02 09:52:35 -0800

    table transformer

--- src/translib/transformer/xfmr_tacacs.go
+++ src/translib/transformer/xfmr_tacacs.go
@@ -1,6 +1,8 @@
 package transformer
 
 import (
+        "errors"
+        log "github.com/golang/glog"
         "github.com/openconfig/ygot/ygot"
         "translib/ocbinds"
 )
@@ -14,6 +16,7 @@ func init() {
     XlateFuncBind("YangToDb_auth_set_key_xfmr", YangToDb_auth_set_key_xfmr)
     XlateFuncBind("YangToDb_server_set_key_xfmr", YangToDb_server_set_key_xfmr)
     XlateFuncBind("YangToDb_global_set_key_xfmr", YangToDb_global_set_key_xfmr)
+    XlateFuncBind("aaa_global_table_xfmr", aaa_global_table_xfmr)
 }
 
 var YangToDb_auth_set_key_xfmr KeyXfmrYangToDb = func(inParams XfmrParams) (string, error) {
@@ -29,5 +32,32 @@ var YangToDb_server_set_key_xfmr KeyXfmrYangToDb = func(inParams XfmrParams) (st
 
 var YangToDb_global_set_key_xfmr KeyXfmrYangToDb = func(inParams XfmrParams) (string, error) {
     return "global", nil
+}
+
+var aaa_global_table_xfmr TableXfmrFunc = func (inParams XfmrParams) ([]string, error) {
+
+    var tblList []string
+    var err error
+
+    log.Infof("aaa_global_table_xfmr - Uri: ", inParams.uri);
+    pathInfo := NewPathInfo(inParams.uri)
+
+    targetUriPath, err := getYangPathFromUri(pathInfo.Path)
+
+    aaaType := pathInfo.Var("name");
+    log.Info("TableXfmrFunc - targetUriPath : ", targetUriPath)
+    log.Info("TableXfmrFunc - type : ", aaaType)
+
+    if (aaaType == "TACACS") {
+        tblList = append(tblList, "TACPLUS_SERVER")
+    } else if (aaaType == "RADIUS") {
+        tblList = append(tblList, "RADIUS")
+    }
+//   else {
+//        err = errors.New("Invalid URI")
+//    }
+
+    log.Infof("TableXfmrFunc - uri(%v), tblList(%v)\r\n", inParams.uri, tblList);
+    return tblList, err
 }
 
