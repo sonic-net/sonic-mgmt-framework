cd2d5a028f697d41746e579b342f0a8e0030e8b3
Syed Amin <obaid.amin@dell.com>
2020-01-17 16:16:31 -0800

    Updated transformer code, removed 'all' leaf from yang and made relevant changes in actioner.

--- src/CLI/actioner/sonic-cli-neighbors.py
+++ src/CLI/actioner/sonic-cli-neighbors.py
@@ -43,11 +43,11 @@ def get_keypath(func,args):
     elif func == 'rpc_sonic_clear_neighbors':
         keypath = cc.Path('/restconf/operations/sonic-neighbor:clear-neighbors')
         if (len (args) == 2):
-            body = {"sonic-neighbor:input":{"family": args[0], "force": args[1], "all": "true", "ip": "", "interface": ""}}
+            body = {"sonic-neighbor:input":{"family": args[0], "force": args[1], "ip": "", "ifname": ""}}
         elif (len (args) == 3):
-            body = {"sonic-neighbor:input":{"family": args[0], "force": args[1], "all": "false", "ip": args[2], "interface": ""}}
+            body = {"sonic-neighbor:input":{"family": args[0], "force": args[1], "ip": args[2], "ifname": ""}}
         elif (len (args) == 4):
-            body = {"sonic-neighbor:input":{"family": args[0], "force": args[1], "all": "false", "ip": "", "interface": args[3]}}
+            body = {"sonic-neighbor:input":{"family": args[0], "force": args[1], "ip": "", "ifname": args[3]}}
 
     return keypath, body
 
@@ -216,7 +216,7 @@ def run(func, args):
         elif 'sonic-neighbor:output' in response.keys():
             status = response['sonic-neighbor:output']
             status = status['response']
-            if (status != ""):
+            if (status != "Success"):
                 print status
             return
         else:
