26564153ee0f1596b23b8378dce1a269ed16427d
Olivier Singla <olivier.singla@broadcom.com>
2020-01-31 12:22:14 -0800

    [JIRA SONIC-16930] openconfig-ztp: admin-status state inconsistent with admin-status config
    We have removed KLISH support for "ztp run" command. We may revisit this option
    later.
    
    Change-Id: I83f005a867b3ae49187193bb4054f280ddcf3a07

--- src/CLI/actioner/sonic_cli_ztp.py
+++ src/CLI/actioner/sonic_cli_ztp.py
@@ -17,27 +17,18 @@ def invoke(func, args):
     if func == 'get_openconfig_ztp_ztp_state':
         path = cc.Path('/restconf/data/openconfig-ztp:ztp/state')
         return aa.get(path)
-    elif func == 'patch_openconfig_ztp_ztp_run':
-        path = cc.Path('/restconf/data/openconfig-ztp:ztp/config')
-        body = {
-                 "openconfig-ztp:config": {
-                 "admin_mode":2
-                 }
-                }
-        api_response = aa.patch(path,body)
-        return api_response
     else:
         path = cc.Path('/restconf/data/openconfig-ztp:ztp/config')
         if 'no' in args:
             body = {
                      "openconfig-ztp:config": {
-                     "admin_mode":0
+                     "admin_mode":False
                      }
                     }
         else:
             body = {
                      "openconfig-ztp:config": {
-                     "admin_mode":1
+                     "admin_mode":True
                      }
                     }
         return aa.patch(path,body)
@@ -53,10 +44,6 @@ def run(func, args):
                    value = response['openconfig-ztp:state']
                    if value is not None:
                        show_cli_output(args[0],value)
-        else:
-            if func == "patch_openconfig_ztp_ztp_run":
-                response = api_response.content['ietf-restconf:errors']['error'][0]['error-message']
-                print("%s" % response)
     except Exception as ex:
         print ex
         print("%Error: Transaction Failure")
