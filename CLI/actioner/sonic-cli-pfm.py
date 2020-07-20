ea924f88c9b455073ed0c12fb0fe5902ebad47e6
Sachin Holla <sachin.holla@broadcom.com>
2020-07-20 18:48:19 +0530

    Migrate platform CLIs to generic REST client

--- CLI/actioner/sonic-cli-pfm.py
+++ CLI/actioner/sonic-cli-pfm.py
@@ -18,20 +18,12 @@
 ###########################################################################
 
 import sys
-import time
-import json
-import ast
-import openconfig_platform_client
+import cli_client as cc
 from rpipe_utils import pipestr
-from openconfig_platform_client.rest import ApiException
 from scripts.render_cli import show_cli_output
 
 
-import urllib3
-urllib3.disable_warnings()
-
 blocked_fields = {'parent':0, 'used_power':0, 'allocated_power':0, 'temperature':0}
-plugins = dict()
 
 def filter_json_value(value):
     for key,val in value.items():
@@ -46,48 +38,19 @@ def filter_json_value(value):
 
     return value
 
-def register(func):
-    """Register sdk client method as a plug-in"""
-    plugins[func.__name__] = func
-    return func
-
-
-def call_method(name, args):
-    method = plugins[name]
-    return method(args)
-
-def generate_body(func, args):
-    body = None
-    # Get the rules of all ACL table entries.
-
-    if func.__name__ == 'get_openconfig_platform_components':
-        keypath = []
-
-    else:
-       body = {}
-
-    return keypath,body
-
+def get_openconfig_platform_components(*args):
+    path = cc.Path('/restconf/data/openconfig-platform:components')
+    return cc.ApiClient().get(path)
 
 def run(func, args):
-    c = openconfig_platform_client.Configuration()
-    c.verify_ssl = False
-    aa = openconfig_platform_client.OpenconfigPlatformApi(api_client=openconfig_platform_client.ApiClient(configuration=c))
+    # lookup and invoke the function name passed by CLI
+    response = globals()[func](*args)
 
-    # create a body block
-    keypath, body = generate_body(func, args)
-
-    try:
-        if body is not None:
-           api_response = getattr(aa,func.__name__)(*keypath, body=body)
-
-        else :
-           api_response = getattr(aa,func.__name__)(*keypath)
-     
+    if response.ok():
+        api_response = response.content
         if api_response is None:
-            print ("Success")
+            return
         else:
-            api_response = aa.api_client.sanitize_for_serialization(api_response)
             value =  api_response['openconfig-platform:components']['component'][0]['state']
 	    if value is None:
                 return
@@ -97,32 +60,14 @@ def run(func, args):
 	            value['oper-status'] = temp[len(temp) - 1]
             show_cli_output(sys.argv[2],filter_json_value(value))
 
-    except ApiException as e:
-        if e.body != "":
-            body = json.loads(e.body)
-            if "ietf-restconf:errors" in body:
-                 err = body["ietf-restconf:errors"]
-                 if "error" in err:
-                     errList = err["error"]
-
-                     errDict = {}
-                     for dict in errList:
-                         for k, v in dict.iteritems():
-                              errDict[k] = v
-
-                     if "error-message" in errDict:
-                         print "%Error: " + errDict["error-message"]
-                         return
-                     print "%Error: Application Failure"
-                     return
-            print "%Error: Application Failure"
-        else:
-            print "Failed"
+    else:
+        print(response.error_message())
+        return 1
 
 if __name__ == '__main__':
 
     pipestr().write(sys.argv)
     #pdb.set_trace()
-    func = eval(sys.argv[1], globals(), openconfig_platform_client.OpenconfigPlatformApi.__dict__)
+    func = sys.argv[1]
     run(func, sys.argv[2:])
 
