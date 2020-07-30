a9ea6f733b61e87d396a2d776074e02ede9a1bb2
Sachin Holla <51310506+sachinholla@users.noreply.github.com>
2020-07-31 05:13:14 +0530

    Generic REST client for CLI (#56)
    Change summary:
    
    Python REST client for CLI
    cli_client.py defines a geneic python REST client for CLI actioners.
    Swagger generated client sdk is bulky and CLI uses very very small
    subset of the APIs it provides. This generic client is lightweight and
    is customized for CLI to management REST server communication.
    
    Sample usage (details are in README_cli_client.md):
    
      api = cli_client.ApiClient()
      path = cli_client.Path('/restconf/data/sonic-port:sonic-port')
      response = api.get(path)
      if response.ok():
          process_data(response.content)
      else:
          print(response.error_message())
    cli_log.py defines APIs to write log messages from CLI actioners.
    Logs are written to syslog. They are also written to screen if env
    variable LOGTOSCREEN is set.
    
    Generate swagger python client sdk for select models
    Disabled automatic generation of swagger client sdk for all yang and
    openapi models. CLI actioners are encouraged to use generic REST client.
    Client sdk generation can be enabled for select models through an entry
    in models/codegen.config file.
    
    Migrate platform CLIs to generic REST client
    Modified show platform CLI actioner to use new Generic REST client
    to demonstrate the usage of new client. Disabled generation of swagger
    client sdk code for openconfig-platform.yang.

---
+++ CLI/actioner/cli_log.py
@@ -0,0 +1,59 @@
+################################################################################
+#                                                                              #
+#  Copyright 2019 Broadcom. The term Broadcom refers to Broadcom Inc. and/or   #
+#  its subsidiaries.                                                           #
+#                                                                              #
+#  Licensed under the Apache License, Version 2.0 (the "License");             #
+#  you may not use this file except in compliance with the License.            #
+#  You may obtain a copy of the License at                                     #
+#                                                                              #
+#     http://www.apache.org/licenses/LICENSE-2.0                               #
+#                                                                              #
+#  Unless required by applicable law or agreed to in writing, software         #
+#  distributed under the License is distributed on an "AS IS" BASIS,           #
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    #
+#  See the License for the specific language governing permissions and         #
+#  limitations under the License.                                              #
+#                                                                              #
+################################################################################
+
+import os
+import syslog
+import inspect
+
+syslog.openlog('sonic-cli')
+__enable_debug = (os.getenv("DEBUG") is not None)
+__enable_print = (os.getenv("LOGTOSCREEN") is not None)
+__severity_str = [ 'EMERG', 'ALERT', 'CRIT', 'ERROR', 'WARN', 'NOTICE', 'INFO', 'DEBUG' ]
+
+
+def log_debug(msg):
+    if __enable_debug:
+        __write_log(syslog.LOG_DEBUG, msg)
+
+def log_info(msg):
+    __write_log(syslog.LOG_INFO, msg)
+
+def log_warning(msg):
+    __write_log(syslog.LOG_WARNING, msg)
+
+def log_error(msg):
+    __write_log(syslog.LOG_ERR, msg)
+
+
+def __write_log(severity, msg):
+    if not isinstance(msg, basestring):
+        msg = str(msg)
+
+    syslog.syslog(severity, msg)
+
+    if __enable_print:
+        caller_frame = inspect.stack()[2][0]
+        frame_info = inspect.getframeinfo(caller_frame)
+        for line in msg.split('\n'):
+            print('[{}:{}/{}] {}:: {}'.format(
+                os.path.basename(frame_info.filename),
+                frame_info.lineno,
+                frame_info.function,
+                __severity_str[severity],
+                line))
