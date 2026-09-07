351b31920513b7c727747017c75a06f7264b1664
Ashutosh Agrawal <ashu@cisco.com>
2026-09-07 11:03:52 -0700

    CLI: verify remote REST server certificates
    Signed-off-by: Ashutosh Agrawal <ashu@cisco.com>

---
+++ tools/test/rest-tls.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+TEST_DIR=$(dirname "$(readlink -f "$0")")
+TEST_BIN=$(mktemp /tmp/rest-tls-test-XXXXXX)
+trap 'rm -f "$TEST_BIN"' EXIT
+
+g++ -std=c++11 -Wall -Wextra -Werror "$TEST_DIR/rest-tls.cpp" -o "$TEST_BIN"
+"$TEST_BIN"
